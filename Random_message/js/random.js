50339a9d9ce7127ac36c1f6c7d602df280ae439c
Mona Lisa <121449911+rocode2@users.noreply.github.com>
2023-02-09 18:51:43 +0100

    finito

--- Random_message/js/random.js
+++ Random_message/js/random.js
@@ -1,9 +1,16 @@
 const messages = [1, 2, 3];
-const debut = ['hey', 'yo', 'Hi'];
+const debut = ['Hey', 'Yo !', 'Hi'];
 const adjectives = ['like', 'hate', 'dislike'];
-const randomNumber = Math.floor(Math.random)*messages.length;
+const randomNumber = Math.floor(Math.random()*messages.length);
+const choice = 0;
+const choice2 = 0;
 
-function printRandomMessages() {
-    return console.log(messages + debut[randomNumber] + ' you, I ' + adjectives[randomNumber] + ' you !')
-
+function printRandomMessages(choice, choice2) {
+    if (choice && choice2){
+        console.log(messages + ' ' + debut[choice] + ' you, I ' + adjectives[choice2] + ' you !')
+    } else {console.log(messages + ' ' + debut[randomNumber] + ' you, I ' + adjectives[randomNumber] + ' you !')}
+   
 }
+
+printRandomMessages(1, 2)
+printRandomMessages()
